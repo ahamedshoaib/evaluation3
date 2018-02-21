d1853f76599b7554aef64fc0f5d7459bb8195822
Shoaib <ahamedshoaibk@gmail.com>
2018-02-21 09:56:41 +0530

    initial commit

---
+++ src/redux/reducers/page.js
@@ -0,0 +1,16 @@
+import {} from '../actions';
+
+const defaultState = {
+  page: 1,
+};
+
+const page = (state = defaultState, action) => {
+  switch (action.type) {
+    default: {
+      return state;
+    }
+  }
+};
+
+export default page;
+
